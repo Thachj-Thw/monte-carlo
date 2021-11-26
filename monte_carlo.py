9938eb2439718322da23bb9cf8964f349db45548
Thw <chanhosatoh@gmail.com>
2021-11-26 22:58:57 +0700

    maximum performance

--- monte_carlo.py
+++ monte_carlo.py
@@ -10,7 +10,7 @@ random.seed(9)
 GAMMA = sqrt(pi)
 
 
-@njit(fastmath=True, cache=True)
+@njit(fastmath=True, cache=True, nogil=True)
 def correctFunction(t: float):
     temp = (4 * t**(3/2))/(3 * GAMMA)
     return temp
@@ -32,10 +32,6 @@ class MCIntegration:
     3.   N uniform distribution
     4.   step deltaX
     5.   number of steps
-    There are two method:
-
-    1.   getIntegral: calculate particular integration
-    2.   getDataset: return a set of integration value for plotting and NN training
     """
     def __init__(self, a: int, b: int, N: int, dx: float, data_count: int):
         self.a = a
@@ -43,49 +39,53 @@ class MCIntegration:
         self.N = N
         self.dx = dx
         self.n = data_count
-    
-    @staticmethod
-    def f(t: float, x: float):
+
+@njit(nogil=True, fastmath=True, cache=True)
+def f(t: float, x: float):
         temp = t / (sqrt(x - t) * GAMMA)
         return temp
 
-    def getIntegral(self):
-        # O(self.N)
-        ar = zeros(self.N)
-        for i in prange(self.N):
-            ar[i] = random.uniform(self.a, self.b)
+# calculate particular integration
+@njit(nogil=True, fastmath=True, cache=True, parallel=True)
+def getIntegral(c: MCIntegration):
+    # O(self.N)
+    ar = zeros(c.N)
+    for i in prange(c.N):
+        ar[i] = random.uniform(c.a, c.b)
+    integral = 0.0
+    for i in ar:
+        integral += f(i, c.b)
+    ans = (c.b - c.a) / float(c.N) * integral
+    return ans
+
+# return a set of integration value for plotting and NN training
+@njit(nogil=True, fastmath=True, cache=True, parallel=True)
+def getDataset(c: MCIntegration):
+    # O(self.n * self.N)
+    store = zeros(c.n)
+    index = zeros(c.n)
+    for y in prange(1, c.n+1):
+        b = c.a + y * c.dx
+        ar = zeros(c.N)
+        for i in prange(c.N):
+            ar[i] = random.uniform(c.a, b)
         integral = 0.0
         for i in ar:
-            integral += self.f(i, self.b)
-        ans = (self.b - self.a) / float(self.N) * integral
-        return ans
-    
-    def getDataset(self):
-        # O(self.n * self.N)
-        store = zeros(self.n)
-        index = zeros(self.n)
-        for y in prange(1, self.n+1):
-            b = self.a + y * self.dx
-            ar = zeros(self.N)
-            for i in prange(self.N):
-                ar[i] = random.uniform(self.a, b)
-            integral = 0.0 
-            for i in ar:
-                integral += self.f(i, b)
-            store[y-1] = (b - self.a) / float(self.N) * integral
-            index[y-1] = b
-        return index, store
+            integral += f(i, b)
+        store[y-1] = (b - c.a) / float(c.N) * integral
+        index[y-1] = b
+    return index, store
 
 
 def main():
     integration = MCIntegration(a=0, b=1, N=1000000, dx=0.001, data_count=1000)
 
-    ans = integration.getIntegral()
+    ans = getIntegral(integration)
     expected = correctFunction(0.5)
     print("The error is:", abs(expected - ans))
     print(ans)
 
-    index, store = integration.getDataset()
+    index, store = getDataset(integration)
     correct = correctFunction(index)
 
     # Export caculated dataSet and correct dataSet to CSV File
